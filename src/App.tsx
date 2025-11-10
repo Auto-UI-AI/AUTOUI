08e1286a786098465a94bd5e663b3c927d7996cb
Makc240305 <maxim240305@gmail.com>
2025-11-10 17:22:47 +0100

    Commented

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import PlayGround from './playground/PlayGround';
 import DemoPage from './demo/DemoPage';
 import { ModalChat } from '@lib';
 import { autouiConfig } from '../autoui.config.example.ts';
+import { InteractiveDemo } from './demo/DemoInteractive.tsx';
 function App() {
   return (
     <>
       <DemoPage />
+      {/* <InteractiveDemo /> */}
       <PlayGround />
       <ModalChat config={autouiConfig}/>
     </>
