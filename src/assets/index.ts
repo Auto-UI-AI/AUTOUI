c62506643de7bc5fcac7ae85fded63bfe6e0fc49
IvanDeresh <dereshivan0@gmail.com>
2025-11-12 01:11:08 +0100

    icon update

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+import Plus from './plus-large-svgrepo-com.svg';
+import Xmark from './xmark-svgrepo-com.svg';
+
+export { Plus, Xmark };
--- src/lib/ui/chat/ui/btnOpenChat/index.tsx
+++ src/lib/ui/chat/ui/btnOpenChat/index.tsx
@@ -1,13 +1,11 @@
 import type { FC } from 'react';
 import type { BtnOpenChatProps } from '@lib/ui/chat/types';
 import clsx from 'clsx';
-import XMarkIcon from '../assets/xmark-svgrepo-com.svg';
-import PlusIcon from '../assets/plus-large-svgrepo-com.svg';
-
+import { Plus, Xmark } from 'src/assets';
 export const BtnOpenChat: FC<BtnOpenChatProps> = ({ onOpenChange, isOpen, className }) => {
   const safeIsOpen = isOpen ?? true;
 
-  const iconSrc = safeIsOpen ? XMarkIcon : PlusIcon;
+  const iconSrc = safeIsOpen ? Xmark : Plus;
 
   return (
     <button onClick={onOpenChange} className={clsx('autoui-chat-open-btn', className)}>
