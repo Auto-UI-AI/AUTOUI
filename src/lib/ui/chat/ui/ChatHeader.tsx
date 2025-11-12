a2ab376bf000205cfbaa3f420f0e233b31314784
IvanDeresh <dereshivan0@gmail.com>
2025-11-12 01:18:20 +0100

    icon update

--- src/lib/ui/chat/ui/ChatHeader.tsx
+++ src/lib/ui/chat/ui/ChatHeader.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { useChatContext } from '../context/chatContext';
-import Xmark from 'src/assets/xmark-svgrepo-com.svg';
+// import Xmark from 'src/assets/xmark-svgrepo-com.svg';
 
 export const ChatHeader = () => {
   const { title, classNames, onClose, closeIcon } = useChatContext();
@@ -12,7 +12,7 @@ export const ChatHeader = () => {
       </h2>
       {onClose && (
         <button role="closeButton" className={clsx('autoui-chat-closebtn', classNames?.closeButton)} onClick={onClose}>
-          {closeIcon ?? <img src={Xmark} alt={'Open char'} width={24} height={24} />}
+          {closeIcon ?? <img src={'src/assets/xmark-svgrepo-com.svg'} alt={'Open char'} width={24} height={24} />}
         </button>
       )}
     </header>
