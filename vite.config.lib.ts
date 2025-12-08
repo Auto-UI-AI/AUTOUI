bfebebc68e3de5589c635e5c15c1a99e020a7f23
IvanDeresh <dereshivan0@gmail.com>
2025-12-08 10:10:09 +0100

    fix: tsconfig.build added

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     dts({
+      tsconfigPath: './tsconfig.build.json',
       include: ['src/lib'],
       insertTypesEntry: true,
     }),
